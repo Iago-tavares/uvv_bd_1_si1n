6107bce3307e19ac0dabfa29bbd76fc221e92a72
Iago <iagost@outlook.com>
2022-11-13 14:04:02 -0300

    ajuste dos nomes

--- PSet 1/si1n_202203891_postgresql_hr.sql.sql
+++ PSet 1/si1n_202203891_postgresql_hr.sql.sql
@@ -7,8 +7,6 @@ CREATE DATABASE uvv
     WITH
     OWNER = iago
     ENCODING = 'UTF8'
-    LC_COLLATE = 'Portuguese_Brazil.1252'
-    LC_CTYPE = 'Portuguese_Brazil.1252'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
